3f97c8529c68ebedde8f1e160df4c2150325b317
jbrunner <brunner.86@osu.edu>
2016-07-25 23:27:00 -0400

    Improved styling and added countdown to header.

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,21 @@ Rails.application.routes.draw do
     root 'sessions#new'
 
     #users
-    get 'users', :to => 'users#index'
-    get 'users/new', :to => 'users#new', :as => 'signup'
-    post 'users', :to => 'users#create'
+    get  'users/new', :to => 'users#new', :as => 'signup'
+    get  'users',     :to => 'users#index'
+    post 'users',     :to => 'users#create'
 
     #sessions
-    get 'sessions/new', :to => 'sessions#new', :as => 'login'
-    post 'sessions/create', :to => 'sessions#create'
-    get 'sessions/destroy', :to => 'sessions#destroy', :as => 'logout'
+    get  'sessions/new',     :to => 'sessions#new',     :as => 'login'
+    get  'sessions/destroy', :to => 'sessions#destroy', :as => 'logout'
+    post 'sessions/create',  :to => 'sessions#create'
 
     #main
-    get 'main', :to => 'main#index', :as => 'home'
-    get 'main/who', :to => 'main#who'
-    get 'main/what', :to => 'main#what'
-    get 'main/when', :to => 'main#when'
-    get 'main/where', :to => 'main#where'
-    get 'main/why', :to => 'main#why'
-    get 'main/how', :to => 'main#how'
+    get 'main',       :to => 'main#index', :as => 'home'
+    get 'main/who',   :to => 'main#who',   :as => 'who'
+    get 'main/what',  :to => 'main#what',  :as => 'what'
+    get 'main/when',  :to => 'main#when',  :as => 'when'
+    get 'main/where', :to => 'main#where', :as => 'where'
+    get 'main/why',   :to => 'main#why',   :as => 'why'
+    get 'main/how',   :to => 'main#how',   :as => 'how'
 end
