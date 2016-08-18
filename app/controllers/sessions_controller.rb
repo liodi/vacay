a7e5a1deb2514317896ac774eba55a8124852de7
jbrunner <brunner.86@osu.edu>
2016-08-18 00:48:04 -0400

    Stop non logged in users from viewing pages. Changed home redirects to who page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
         @user = User.find_by(:email => params[:email])
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
-            redirect_to home_path
+            redirect_to who_path
         else
             render 'new'
         end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            redirect_to home_path
+            redirect_to who_path
         else
             render 'new'
         end
